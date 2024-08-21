704cbd46cdade899b7e4eba7a7dcabf5afb5486a
notvalproate <sushruth127@gmail.com>
2024-08-21 11:44:09 +0530

    Using router + added landing page component and made some folders

--- chowka-bara-client/src/app/app.component.ts
+++ chowka-bara-client/src/app/app.component.ts
@@ -2,18 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
-import { AnimatedSidePatternComponent } from './animated-side-pattern/animated-side-pattern.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { JoinRoomComponent } from './join-room/join-room.component';
-
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, AnimatedSidePatternComponent, HeaderComponent, FooterComponent, JoinRoomComponent],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'chowka-bara-client';
+  
 }
---
+++ chowka-bara-client/src/app/landing-page/landing-page.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AnimatedSidePatternComponent } from '../animated-side-pattern/animated-side-pattern.component';
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
+import { JoinRoomComponent } from '../join-room/join-room.component';
+
+@Component({
+  selector: 'app-landing-page',
+  standalone: true,
+  imports: [CommonModule, AnimatedSidePatternComponent, HeaderComponent, FooterComponent, JoinRoomComponent],
+  templateUrl: './landing-page.component.html',
+  styleUrl: './landing-page.component.scss'
+})
+export class LandingPageComponent {
+
+}
